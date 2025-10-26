70ab08585c382bebe3aebf3d2e3789607117e071
Eurii-Yurii <euriiyurii@gmail.com>
2025-10-26 21:43:35 +0800

    Update whackAMole_ni_Kirara.cpp

--- WhackAMole ni Kirara/whackAMole_ni_Kirara.cpp
+++ WhackAMole ni Kirara/whackAMole_ni_Kirara.cpp
@@ -3,22 +3,41 @@
 #include <iostream>
 using namespace std;
 
+void button() {
 
-void button(int posX, int posY, int length, int height) {
-
+    glColor3f(100,100,100);
     glBegin(GL_QUADS);
-    glVertex2f(posX, posY);
-    glVertex2f(posX, (posY - height));
-    glVertex2f((posX - (-length)), (posY - height));
-    glVertex2f((posX - (-length)), posY);
+    glVertex3f(1, 1, 1);
+    glVertex3f(5, 1, 1);
+    glVertex3f(5, 5, 1);
+    glVertex3f(1, 5, 1);
+    // Back
+    glVertex3f(1, 1, 0);
+    glVertex3f(5, 1, 0);
+    glVertex3f(5, 5, 0);
+    glVertex3f(1, 5, 0);
+    // Left
+    glVertex3f(1, 1, 0);
+    glVertex3f(1, 1, 1);
+    glVertex3f(1, 5, 1);
+    glVertex3f(1, 5, 0);
+    // Right
+    glVertex3f(5, 1, 0);
+    glVertex3f(5, 1, 1);
+    glVertex3f(5, 5, 1);
+    glVertex3f(5, 5, 0);
+    // Top
+    glVertex3f(1, 5, 0);
+    glVertex3f(5, 5, 0);
+    glVertex3f(5, 5, 1);
+    glVertex3f(1, 5, 1);
+    // Bottom
+    glVertex3f(1, 1, 0);
+    glVertex3f(5, 1, 0);
+    glVertex3f(5, 1, 1);
+    glVertex3f(1, 1, 1);
     glEnd();
 
-    glBegin(GL_LINES);
-    glVertex2f((posX - (-3)), posY);
-    glVertex2f((posX - (-3)), (posY - (-5)));
-    glVertex2f((posX - (-17)), (posY - (-5)));
-    glVertex2f((posX - (-17)), posY);
-    glEnd();
 }
 
 void RenderScene(void) {
@@ -26,47 +45,44 @@ void RenderScene(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    gluLookAt(0.0f, 0.0f, 100.0f,
+    gluLookAt(
+        0.0f, -20.0f, 15.0f,
         0.0f, 0.0f, 0.0f,
-        0.0f, 1.0f, 0.0f);
+        0.0f, 10.0f, 0.0f
+    );
 
-    button(-55, 24, 20, 3);
+    button();
+
 
     glutSwapBuffers();
 }
 
 void ChangeSize(int w, int h) {
-    //height or width should not be equal to 0
+
     if (h == 0)
         h = 1;
     float ratio = 1.0 * w / h;
-    //fix the matrixmode to projection 
+
     glMatrixMode(GL_PROJECTION);
-    //Reset the value of matrices
     glLoadIdentity();
-    //Set the viewport of the window
     glViewport(0, 0, w, h);
     gluPerspective(45, ratio, 1, 1000);
-    // Get Back to Model View
     glMatrixMode(GL_MODELVIEW);
 }
 
 int main(int argc, char** argv)
 {
-    //HelloOpenGL, DoSomething, DoSomething2
-   //init GLUT and create a window
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowPosition(100, 100);
     glutInitWindowSize(1080, 720);
     glutCreateWindow("MyOpenGL");
-    //register callbacks
+
     glutDisplayFunc(RenderScene);
     glutReshapeFunc(ChangeSize);
     glutIdleFunc(RenderScene);
 
     glEnable(GL_DEPTH_TEST);
-    // enter GLUT event processing cycle
     glutMainLoop();
     return 1;
 }
