17acb1c4403ba225676b38be43e50dc4f16923ce
Eurii-Yurii <euriiyurii@gmail.com>
2025-10-27 20:25:01 +0800

    Update whackAMole_ni_Kirara.cpp

--- WhackAMole ni Kirara/whackAMole_ni_Kirara.cpp
+++ WhackAMole ni Kirara/whackAMole_ni_Kirara.cpp
@@ -122,6 +122,16 @@ int main(int argc, char** argv)
     glutIdleFunc(RenderScene);
 
     glEnable(GL_DEPTH_TEST);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_COLOR_MATERIAL);
+
+    GLfloat light_pos[] = { 0.0f, 20, 15.0f };
+    glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+
+    glClearColor(0.1, 0.1, 0.1, 1.0);
+
     glutMainLoop();
     return 1;
 }
